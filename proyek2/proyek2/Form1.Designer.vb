83a2ab42446df70328558a5f67cd02ffd07c0da9
Putri Kartika <putri.kartika96@gmail.com>
2015-10-10 18:40:37 +0700

    tugaskalkulator_putrikartika

---
+++ proyek2/proyek2/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.angka0 = New System.Windows.Forms.Button()
+        Me.samadengan = New System.Windows.Forms.Button()
+        Me.tambah = New System.Windows.Forms.Button()
+        Me.angka1 = New System.Windows.Forms.Button()
+        Me.angka2 = New System.Windows.Forms.Button()
+        Me.angka3 = New System.Windows.Forms.Button()
+        Me.kurang = New System.Windows.Forms.Button()
+        Me.angka4 = New System.Windows.Forms.Button()
+        Me.angka5 = New System.Windows.Forms.Button()
+        Me.angka6 = New System.Windows.Forms.Button()
+        Me.hapus = New System.Windows.Forms.Button()
+        Me.angka7 = New System.Windows.Forms.Button()
+        Me.angka8 = New System.Windows.Forms.Button()
+        Me.angka9 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(23, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(238, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        '
+        'angka0
+        '
+        Me.angka0.Location = New System.Drawing.Point(23, 201)
+        Me.angka0.Name = "angka0"
+        Me.angka0.Size = New System.Drawing.Size(45, 38)
+        Me.angka0.TabIndex = 1
+        Me.angka0.Text = "0"
+        Me.angka0.UseVisualStyleBackColor = True
+        '
+        'samadengan
+        '
+        Me.samadengan.Location = New System.Drawing.Point(90, 201)
+        Me.samadengan.Name = "samadengan"
+        Me.samadengan.Size = New System.Drawing.Size(100, 38)
+        Me.samadengan.TabIndex = 2
+        Me.samadengan.Text = "="
+        Me.samadengan.UseVisualStyleBackColor = True
+        '
+        'tambah
+        '
+        Me.tambah.Location = New System.Drawing.Point(218, 201)
+        Me.tambah.Name = "tambah"
+        Me.tambah.Size = New System.Drawing.Size(43, 38)
+        Me.tambah.TabIndex = 3
+        Me.tambah.Text = "+"
+        Me.tambah.UseVisualStyleBackColor = True
+        '
+        'angka1
+        '
+        Me.angka1.Location = New System.Drawing.Point(23, 153)
+        Me.angka1.Name = "angka1"
+        Me.angka1.Size = New System.Drawing.Size(45, 42)
+        Me.angka1.TabIndex = 4
+        Me.angka1.Text = "1"
+        Me.angka1.UseVisualStyleBackColor = True
+        '
+        'angka2
+        '
+        Me.angka2.Location = New System.Drawing.Point(90, 153)
+        Me.angka2.Name = "angka2"
+        Me.angka2.Size = New System.Drawing.Size(43, 42)
+        Me.angka2.TabIndex = 5
+        Me.angka2.Text = "2"
+        Me.angka2.UseVisualStyleBackColor = True
+        '
+        'angka3
+        '
+        Me.angka3.Location = New System.Drawing.Point(146, 153)
+        Me.angka3.Name = "angka3"
+        Me.angka3.Size = New System.Drawing.Size(44, 41)
+        Me.angka3.TabIndex = 6
+        Me.angka3.Text = "3"
+        Me.angka3.UseVisualStyleBackColor = True
+        '
+        'kurang
+        '
+        Me.kurang.Location = New System.Drawing.Point(218, 153)
+        Me.kurang.Name = "kurang"
+        Me.kurang.Size = New System.Drawing.Size(43, 41)
+        Me.kurang.TabIndex = 7
+        Me.kurang.Text = "-"
+        Me.kurang.UseVisualStyleBackColor = True
+        '
+        'angka4
+        '
+        Me.angka4.Location = New System.Drawing.Point(23, 109)
+        Me.angka4.Name = "angka4"
+        Me.angka4.Size = New System.Drawing.Size(45, 38)
+        Me.angka4.TabIndex = 8
+        Me.angka4.Text = "4"
+        Me.angka4.UseVisualStyleBackColor = True
+        '
+        'angka5
+        '
+        Me.angka5.Location = New System.Drawing.Point(90, 109)
+        Me.angka5.Name = "angka5"
+        Me.angka5.Size = New System.Drawing.Size(43, 38)
+        Me.angka5.TabIndex = 9
+        Me.angka5.Text = "5"
+        Me.angka5.UseVisualStyleBackColor = True
+        '
+        'angka6
+        '
+        Me.angka6.Location = New System.Drawing.Point(146, 109)
+        Me.angka6.Name = "angka6"
+        Me.angka6.Size = New System.Drawing.Size(44, 38)
+        Me.angka6.TabIndex = 10
+        Me.angka6.Text = "6"
+        Me.angka6.UseVisualStyleBackColor = True
+        '
+        'hapus
+        '
+        Me.hapus.Location = New System.Drawing.Point(218, 66)
+        Me.hapus.Name = "hapus"
+        Me.hapus.Size = New System.Drawing.Size(47, 81)
+        Me.hapus.TabIndex = 11
+        Me.hapus.Text = "clear"
+        Me.hapus.UseVisualStyleBackColor = True
+        '
+        'angka7
+        '
+        Me.angka7.Location = New System.Drawing.Point(23, 66)
+        Me.angka7.Name = "angka7"
+        Me.angka7.Size = New System.Drawing.Size(45, 37)
+        Me.angka7.TabIndex = 12
+        Me.angka7.Text = "7"
+        Me.angka7.UseVisualStyleBackColor = True
+        '
+        'angka8
+        '
+        Me.angka8.Location = New System.Drawing.Point(90, 66)
+        Me.angka8.Name = "angka8"
+        Me.angka8.Size = New System.Drawing.Size(43, 34)
+        Me.angka8.TabIndex = 13
+        Me.angka8.Text = "8"
+        Me.angka8.UseVisualStyleBackColor = True
+        '
+        'angka9
+        '
+        Me.angka9.Location = New System.Drawing.Point(148, 66)
+        Me.angka9.Name = "angka9"
+        Me.angka9.Size = New System.Drawing.Size(42, 34)
+        Me.angka9.TabIndex = 14
+        Me.angka9.Text = "9"
+        Me.angka9.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.angka9)
+        Me.Controls.Add(Me.angka8)
+        Me.Controls.Add(Me.angka7)
+        Me.Controls.Add(Me.hapus)
+        Me.Controls.Add(Me.angka6)
+        Me.Controls.Add(Me.angka5)
+        Me.Controls.Add(Me.angka4)
+        Me.Controls.Add(Me.kurang)
+        Me.Controls.Add(Me.angka3)
+        Me.Controls.Add(Me.angka2)
+        Me.Controls.Add(Me.angka1)
+        Me.Controls.Add(Me.tambah)
+        Me.Controls.Add(Me.samadengan)
+        Me.Controls.Add(Me.angka0)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents angka0 As System.Windows.Forms.Button
+    Friend WithEvents samadengan As System.Windows.Forms.Button
+    Friend WithEvents tambah As System.Windows.Forms.Button
+    Friend WithEvents angka1 As System.Windows.Forms.Button
+    Friend WithEvents angka2 As System.Windows.Forms.Button
+    Friend WithEvents angka3 As System.Windows.Forms.Button
+    Friend WithEvents kurang As System.Windows.Forms.Button
+    Friend WithEvents angka4 As System.Windows.Forms.Button
+    Friend WithEvents angka5 As System.Windows.Forms.Button
+    Friend WithEvents angka6 As System.Windows.Forms.Button
+    Friend WithEvents hapus As System.Windows.Forms.Button
+    Friend WithEvents angka7 As System.Windows.Forms.Button
+    Friend WithEvents angka8 As System.Windows.Forms.Button
+    Friend WithEvents angka9 As System.Windows.Forms.Button
+
+End Class
---
+++ proyek2/proyek2/Form1.vb
@@ -0,0 +1,110 @@
+﻿Public Class Form1
+    Dim input_angka As Integer
+    Dim rumus As String
+    Dim hasil As Byte
+
+    Private Sub angka0_Click(sender As Object, e As EventArgs) Handles angka0.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text = TextBox1.Text & "0"
+        End If
+    End Sub
+
+    Private Sub angka1_Click(sender As Object, e As EventArgs) Handles angka1.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "1"
+        Else
+            TextBox1.Text = TextBox1.Text & "1"
+        End If
+    End Sub
+
+    Private Sub angka2_Click(sender As Object, e As EventArgs) Handles angka2.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "2"
+        Else
+            TextBox1.Text = TextBox1.Text & "2"
+        End If
+    End Sub
+
+    Private Sub angka3_Click(sender As Object, e As EventArgs) Handles angka3.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "3"
+        Else
+            TextBox1.Text = TextBox1.Text & "3"
+        End If
+    End Sub
+
+    Private Sub angka4_Click(sender As Object, e As EventArgs) Handles angka4.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "4"
+        Else
+            TextBox1.Text = TextBox1.Text & "4"
+        End If
+    End Sub
+
+    Private Sub angka5_Click(sender As Object, e As EventArgs) Handles angka5.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "5"
+        Else
+            TextBox1.Text = TextBox1.Text & "5"
+        End If
+    End Sub
+
+    Private Sub angka6_Click(sender As Object, e As EventArgs) Handles angka6.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "6"
+        Else
+            TextBox1.Text = TextBox1.Text & "6"
+        End If
+    End Sub
+
+    Private Sub angka7_Click(sender As Object, e As EventArgs) Handles angka7.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "7"
+        Else
+            TextBox1.Text = TextBox1.Text & "7"
+        End If
+    End Sub
+
+    Private Sub angka8_Click(sender As Object, e As EventArgs) Handles angka8.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "8"
+        Else
+            TextBox1.Text = TextBox1.Text & "8"
+        End If
+    End Sub
+
+    Private Sub angka9_Click(sender As Object, e As EventArgs) Handles angka9.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "9"
+        Else
+            TextBox1.Text = TextBox1.Text & "9"
+        End If
+    End Sub
+
+    Private Sub hapus_Click(sender As Object, e As EventArgs) Handles hapus.Click
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub kurang_Click(sender As Object, e As EventArgs) Handles kurang.Click
+        input_angka = Val(TextBox1.Text)
+        rumus = "-"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub tambah_Click(sender As Object, e As EventArgs) Handles tambah.Click
+        input_angka = Val(TextBox1.Text)
+        rumus = "+"
+        TextBox1.Text = Nothing
+    End Sub
+
+    Private Sub samadengan_Click(sender As Object, e As EventArgs) Handles samadengan.Click
+        Select Case rumus
+            Case "-"
+                TextBox1.Text = input_angka - Val(TextBox1.Text)
+            Case "+"
+                TextBox1.Text = input_angka + Val(TextBox1.Text)
+        End Select
+    End Sub
+End Class
